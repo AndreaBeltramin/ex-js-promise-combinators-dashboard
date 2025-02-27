05c34caad3c39041d03e725d566c066ee8fdced5
AndreaBeltramin <andreabeltra99@gmail.com>
2025-02-27 20:03:06 +0100

    funzione corretta

--- main.js
+++ main.js
@@ -7,40 +7,40 @@
 // 3. Il nome dell’aeroporto principale da /airports?search={query}
 // (result.name nella nuova proprietà airport).
 
-const getDashboardData = async (query) => {
-	let promises;
+async function getDashboardData(query) {
 	const destinationPromise = fetch(
-		`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=[query]`
-	).then((res) => res.json);
+		`https://boolean-spec-frontend.vercel.app/freetestapi/destinations?search=${query}`
+	).then((res) => res.json());
 
 	const weatherPromise = fetch(
-		`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=[query]`
-	).then((res) => res.json);
+		`https://boolean-spec-frontend.vercel.app/freetestapi/weathers?search=${query}`
+	).then((res) => res.json());
 
 	const airportNamePromise = fetch(
-		`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=[query]`
-	).then((res) => res.json);
+		`https://boolean-spec-frontend.vercel.app/freetestapi/airports?search=${query}`
+	).then((res) => res.json());
 
-	promises = [destinationPromise, weatherPromise, airportNamePromise];
-	const results = await Promise.all(promises);
-	return results;
-};
+	const promises = [destinationPromise, weatherPromise, airportNamePromise];
+	const [destination, weather, airport] = await Promise.all(promises);
+	return {};
+}
 
 // (async () => {
 // 	const result = await Promise.all(results);
 // 	console.log(result);
 // })();
 
-getDashboardData("london")
-	.then((data) => {
-		console.log("Dasboard data:", data);
-		console.log(
-			`${data.city} is in ${data.country}.\n` +
-				`Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
-				`The main airport is ${data.airport}.\n`
-		);
-	})
-	.catch((error) => console.error(error));
+getDashboardData("london");
+
+// .then((data) => {
+// 	console.log("Dasboard data:", data);
+// 	console.log(
+// 		`${data.city} is in ${data.country}.\n` +
+// 			`Today there are ${data.temperature} degrees and the weather is ${data.weather}.\n` +
+// 			`The main airport is ${data.airport}.\n`
+// 	);
+// })
+// .catch((error) => console.error(error));
 
 // Utilizzerai Promise.all() per eseguire queste richieste in parallelo
 // e poi restituirai un oggetto con i dati aggregati.
